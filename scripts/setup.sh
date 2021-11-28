ea4398a5f6c2e4c331c8e113d0cfe579faca01cb
Damien Snyder <signup@damiensnyder.com>
2021-11-27 19:54:14 -0800

    add some scripts for setting up and running the serverr

---
+++ scripts/setup.sh
@@ -0,0 +1,11 @@
+# these are the commands i entered to set up the DigitalOcean droplet.
+# after running these, i connected via FTP to send the whole directory with
+# this repo over.
+apt-get update
+apt-get install filezilla
+apt install python3-pip
+Y
+pip install django
+pip install markdown
+pip install markdown-katex
+pip install markdown-customblocks
---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+# starts the server for production.
+# run this from the base directory (i.e., `damiens-site-3`). type
+#     sh scripts/start.sh
+# to run it
+python3 manage.py runserver
