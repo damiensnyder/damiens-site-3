b67e5effe2cbba06b3b91e82ff01f0b6f84d2627
Damien Snyder <damien@damiensnyder.com>
2021-12-01 00:56:57 +0000

    relearn how shell scripts work

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,6 @@
 # run this from the base directory (i.e., `damiens-site-3`). type
 #     sh scripts/start.sh
 # to run it
-python3 manage.py collectstatic
-yes
+echo "yes" > python3 manage.py collectstatic
 /etc/init.d/nginx restart
 uwsgi --socket damiens-site-3.sock --module damienssite3.wsgi --chmod-socket=666 --daemonize=/var/log/uwsgi/damiens-site-3.log
