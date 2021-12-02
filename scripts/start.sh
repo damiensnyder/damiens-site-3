18a989a21208c2b142b4b341a72606f4033ac3ce
Damien Snyder <damien@damiensnyder.com>
2021-12-02 05:53:28 +0000

    kill old uwsgi processes when restarting server

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 # run this from the base directory (i.e., `damiens-site-3`). type
 #     sh scripts/start.sh
 # to run it
-echo "yes" | python3 manage.py collectstatic
+kill -9 `ps aux | grep uwsgi | grep -v grep | awk '{print $2}'`
+python3 manage.py collectstatic --no-input
 /etc/init.d/nginx restart
 uwsgi --socket damiens-site-3.sock --module damienssite3.wsgi --chmod-socket=666 --daemonize=/var/log/uwsgi/damiens-site-3.log
