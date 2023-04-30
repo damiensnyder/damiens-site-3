d4184f8206e0be29642756799e3ca94d8b1e0879
Damien Snyder <53230081+damiensnyder@users.noreply.github.com>
2023-04-30 16:14:31 -0700

    move content static items one level up in path

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,10 +2,14 @@
 # after running these, i connected via FTP to send the whole directory with
 # this repo over.
 apt-get update
-apt-get install filezilla
+apt-get install python3-venv
 apt install python3-pip
 Y
+python3 -m venv venv
+pip install uwsgi
 pip install django
 pip install markdown
 pip install markdown-katex
 pip install markdown-customblocks
+apt-get install nginx
+/etc/init.d/nginx start
