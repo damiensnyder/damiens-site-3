50d6e5f13778b438314fb1bcc78d1c47d6d4d783
Damien Snyder <signup@damiensnyder.com>
2025-03-04 11:16:05 -0500

    improve admin site

--- content/admin.py
+++ content/admin.py
@@ -2,7 +2,51 @@ from django.contrib import admin
 from .models import Tag, Content, Shortform, Message
 
 
-admin.site.register(Tag)
-admin.site.register(Content)
-admin.site.register(Shortform)
-admin.site.register(Message)
+class ExcludeSpamFilter(admin.SimpleListFilter):
+    title = "exclude spam"
+
+    parameter_name = "exclude"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("exclude", "exclude"),
+        ]
+
+    def queryset(self, request, queryset):
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == "exclude":
+            return queryset.exclude(
+                from_content__url="accounts-announcement",
+            )
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('url', 'name')
+    search_fields = ('url__icontains', 'name__icontains')
+
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('url', 'title', 'timestamp', 'primary_tag', 'group_needed')
+    list_filter = ('tags__name', 'group_needed')
+    search_fields = ('url__icontains', 'title__icontains')
+    exclude = ('markup',)
+
+
+class ShortformAdmin(admin.ModelAdmin):
+    list_display = ('url', 'title', 'timestamp', 'primary_tag', 'group_needed')
+    list_filter = ('primary_tag__name', 'group_needed')
+    search_fields = ('url__icontains', 'title__icontains', 'body__icontains')
+    exclude = ('markup',)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'from_content', 'from_shortform', 'timestamp', 'body')
+    list_filter = (ExcludeSpamFilter, 'user', 'from_content')
+    search_fields = ('body__icontains',)
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Content, ContentAdmin)
+admin.site.register(Shortform, ShortformAdmin)
+admin.site.register(Message, MessageAdmin)
--- flags/admin.py
+++ flags/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Flag, Vote
 
 
-admin.site.register(Flag)
-admin.site.register(Vote)
+class FlagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'img_url', 'width', 'height', 'source', 'total_score', 'num_votes')
+    list_filter = ('num_votes', 'total_score')
+    search_fields = ('name__icontains',)
+
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('matchup_id', 'flag', 'user', 'score')
+    list_filter = ('score',)
+    search_fields = ('flag__name__icontains', 'user__username__icontains')
+
+
+admin.site.register(Flag, FlagAdmin)
+admin.site.register(Vote, VoteAdmin)
