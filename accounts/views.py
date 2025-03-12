b19445b6f53b1a8c5e9bd0f9c240ce104396e8ad
Damien Snyder <signup@damiensnyder.com>
2025-03-12 01:28:26 -0400

    work on password changing

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 
+def set_user_password(request):
+    if request.user.is_staff:
+        if request.method == 'POST':
+            user_id = request.POST.get('username')
+            new_password = request.POST.get('password')
+            change_password(user_id, new_password)
+        return render(request, 'accounts/set-user-password.html')
+    return render(request, 'content/illegal-hidden-access.html')
+
+
+def change_password(user_id, new_password):
+    user = User.objects.get(id=user_id)
+    user.password = make_password(new_password)
+    user.save()
+    return user
